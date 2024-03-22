8bccf06d5fc9a84a6f630229271ba65e827104bc
tejaboy <lwkbite@gmail.com>
2024-03-22 10:07:08 +0800

    Convert indendation to tab 4.

--- packages/server/src/entities/Player.ts
+++ packages/server/src/entities/Player.ts
@@ -3,44 +3,44 @@ import {Schema, type} from '@colyseus/schema';
 export type TPlayerOptions = Pick<Player, 'sessionId' | 'userId' | 'name' | 'avatarUri' | 'talking' | 'spriteId' | 'teamId'>;
 
 export class Player extends Schema {
-  @type('string')
-  public sessionId: string;
+	@type('string')
+	public sessionId: string;
 
-  @type('string')
-  public userId: string;
+	@type('string')
+	public userId: string;
 
-  @type('string')
-  public avatarUri: string;
+	@type('string')
+	public avatarUri: string;
 
-  @type('string')
-  public name: string;
+	@type('string')
+	public name: string;
 
-  @type('boolean')
-  public talking: boolean = false;
+	@type('boolean')
+	public talking: boolean = false;
 
-  @type("boolean")
-  public ready: boolean = false;
+	@type("boolean")
+	public ready: boolean = false;
 
-  @type("number")
-  public bulletLeft: number = 60;
+	@type("number")
+	public bulletLeft: number = 60;
 
-  @type("number")
-  public reloadTimer: number = 0;
+	@type("number")
+	public reloadTimer: number = 0;
 
-  @type("number")
-  public teamId: number = 0;
+	@type("number")
+	public teamId: number = 0;
 
-  @type("number")
-  public spriteId: number = 1;
+	@type("number")
+	public spriteId: number = 1;
 
-  // Init
-  constructor({name, userId, avatarUri, sessionId, teamId, spriteId}: TPlayerOptions) {
-    super();
-    this.userId = userId;
-    this.avatarUri = avatarUri;
-    this.name = name;
-    this.sessionId = sessionId;
-    this.teamId = teamId;
-    this.spriteId = spriteId;
-  }
+	// Init
+	constructor({name, userId, avatarUri, sessionId, teamId, spriteId}: TPlayerOptions) {
+		super();
+		this.userId = userId;
+		this.avatarUri = avatarUri;
+		this.name = name;
+		this.sessionId = sessionId;
+		this.teamId = teamId;
+		this.spriteId = spriteId;
+	}
 }
--- packages/server/src/rooms/StateHandlerRoom.ts
+++ packages/server/src/rooms/StateHandlerRoom.ts
@@ -3,38 +3,38 @@ import {TPlayerOptions} from '../entities/Player';
 import {State, IState} from '../entities/State';
 
 export class StateHandlerRoom extends Room<State> {
-  maxClients = 1000;
+	maxClients = 1000;
 
-  onCreate(options: IState) {
-    this.setState(new State(options));
+	onCreate(options: IState) {
+		this.setState(new State(options));
 
-    // Here's where we would add handlers for updating state
-    this.onMessage('startTalking', (client, _data) => {
-      this.state.startTalking(client.sessionId);
-    });
+		// Here's where we would add handlers for updating state
+		this.onMessage('startTalking', (client, _data) => {
+			this.state.startTalking(client.sessionId);
+		});
 
-    this.onMessage('stopTalking', (client, _data) => {
-      this.state.stopTalking(client.sessionId);
-    });
+		this.onMessage('stopTalking', (client, _data) => {
+			this.state.stopTalking(client.sessionId);
+		});
 
-    this.onMessage('ready', (client, _data) => {
-      this.state.setPlayerReady(client.sessionId);
-    })
-  }
+		this.onMessage('ready', (client, _data) => {
+			this.state.setPlayerReady(client.sessionId);
+		})
+	}
 
-  onAuth(_client: any, _options: any, _req: any) {
-    return true;
-  }
+	onAuth(_client: any, _options: any, _req: any) {
+		return true;
+	}
 
-  onJoin(client: Client, options: TPlayerOptions) {
-    this.state.createPlayer(client.sessionId, options);
-  }
+	onJoin(client: Client, options: TPlayerOptions) {
+		this.state.createPlayer(client.sessionId, options);
+	}
 
-  onLeave(client: Client) {
-    this.state.removePlayer(client.sessionId);
-  }
+	onLeave(client: Client) {
+		this.state.removePlayer(client.sessionId);
+	}
 
-  onDispose() {
-    console.log('Dispose StateHandlerRoom');
-  }
+	onDispose() {
+		console.log('Dispose StateHandlerRoom');
+	}
 }
